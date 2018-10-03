ccac51e2963a2cdcfd0eda5a68e042d279190e18
nichow <nhgarcia27@gmail.com>
2018-10-03 19:06:37 -0400

    basic wanderers finished

--- Artificial Intelligence/Assets/Scripts/WanderScript.cs
+++ Artificial Intelligence/Assets/Scripts/WanderScript.cs
@@ -7,13 +7,15 @@ using UnityEngine;
 public class WanderScript : MonoBehaviour {
 
     /* Time variables */
-    private float _maxWanderDuration = 10f; //max amount of time the actor can move before waiting
+    private float _maxWanderDuration = 3f; //max amount of time the actor can move before waiting
     private float _elapsedWalk = 0f;        //amount of time elapsed since the walk began
     private float _maxWaitTime = 3f;        //max amount of time the actor can wait before walking again
     private float _elapsedWait;             //amount of time elapsed whlie waiting
-    /* Direction variables */
+    /* Direction and speed variables */
     private float _randX;
     private float _randZ;
+    private float _speed;
+    private Vector3 _direction;
 
     private bool _isMoving = false;         //true if moving, false otherwise
 	// Use this for initialization
@@ -30,11 +32,53 @@ public class WanderScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+        /* if the actor is not moving and our wait time has reached
+         * the maximum then start the process of moving in a random direction */
 	    if (!_isMoving && _elapsedWait >= _maxWaitTime)
 	    {
-	        var direction = new Vector3(_randX, 0f, _randZ).normalized;
-	        _elapsedWait = 0;
+	        _direction = new Vector3(_randX, 0f, _randZ).normalized;
+	        _speed = .005f;
+            _elapsedWait = 0;
 	        _isMoving = true;
-	    }
+	        StopAllCoroutines();
+	        StartCoroutine(ElapseTime(_isMoving));
+        }
+        /* otherwise if the actor is moving and their walk time has reached
+         * its maximum, start the waiting process and halt their movement
+         * additionally, reset the random direction */
+	    else if (_isMoving && _elapsedWalk >= _maxWanderDuration)
+	    {
+	        _direction = Vector3.zero;
+	        _speed = 0;
+	        _elapsedWalk = 0;
+	        _isMoving = false;
+	        _randX = Random.Range(-1f, 1f);
+	        _randZ = Random.Range(-1f, 1f);
+            StopAllCoroutines();
+	        StartCoroutine(ElapseTime(_isMoving));
+        }
+        transform.Translate(_direction * _speed);
 	}
+
+    IEnumerator ElapseTime(bool which)
+    {
+        /* depending on which Time we're elapsing, iterate
+         * one of the time counters */
+        if (which)
+        {
+            while (_elapsedWalk < _maxWanderDuration)
+            {
+                _elapsedWalk += 1;
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        else
+        {
+            while (_elapsedWait < _maxWaitTime)
+            {
+                _elapsedWait += 1;
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
 }
