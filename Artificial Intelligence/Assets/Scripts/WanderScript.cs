3210d504c1a643388fe206a612c16b7f7562a403
nichow <nhgarcia27@gmail.com>
2018-10-08 21:23:13 -0400

    refactor for consistency's sake and make wait/wander durations more randomized

--- Artificial Intelligence/Assets/Scripts/WanderScript.cs
+++ Artificial Intelligence/Assets/Scripts/WanderScript.cs
@@ -6,11 +6,18 @@ using UnityEngine;
  * them wander aimlessly throughout the scene */
 public class WanderScript : MonoBehaviour {
 
-    /* Time variables */
-    private float _maxWanderDuration = 3f; //max amount of time the actor can move before waiting
-    private float _elapsedWalk = 0f;        //amount of time elapsed since the walk began
-    private float _maxWaitTime = 3f;        //max amount of time the actor can wait before walking again
+    /* Time constants */
+    private const float MinWanderDuration = 1f;  //min amount of time the actor can move before waiting
+    private const float MaxWanderDuration = 5f;  //max amount of time the actor can move before waiting
+    private const float MinWaitDuration = .5f;   //min amount of time the actor can wait before walking
+    private const float MaxWaitDuration = 3f;    //max amount of time the actor can wait before walking
+    
+    /* time variables */
+    private float _wanderDuration;          //actual amount of time spent wandering
+    private float _elapsedWander;           //amount of time elapsed since the walk began, init to 0
+    private float _waitDuration;            //actual amount of time spent waiting
     private float _elapsedWait;             //amount of time elapsed whlie waiting
+
     /* Direction and speed variables */
     private float _randX;
     private float _randZ;
@@ -18,14 +25,18 @@ public class WanderScript : MonoBehaviour {
     private Vector3 _direction;
 
     private bool _isMoving = false;         //true if moving, false otherwise
+
 	// Use this for initialization
 	void Start ()
 	{
         /* initialize our directions to random values */
 	    _randX = Random.Range(-1f, 1f);
 	    _randZ = Random.Range(-1f, 1f);
-        /* initialize elapsed wait to max wait so the actors start wandering */
-	    _elapsedWait = _maxWaitTime;
+        /* initialize our durations */
+	    _wanderDuration = Random.Range(MinWanderDuration, MaxWanderDuration);
+	    _waitDuration = Random.Range(MinWaitDuration, MaxWaitDuration);
+        /* initialize elapsed wait to wait so the actors start wandering */
+	    _elapsedWait = _waitDuration;
 
 	}
 	
@@ -34,10 +45,11 @@ public class WanderScript : MonoBehaviour {
 	{
         /* if the actor is not moving and our wait time has reached
          * the maximum then start the process of moving in a random direction */
-	    if (!_isMoving && _elapsedWait >= _maxWaitTime)
+	    if (!_isMoving && _elapsedWait >= _waitDuration)
 	    {
-	        _direction = new Vector3(_randX, 0f, _randZ).normalized;
-	        _speed = .005f;
+	        _wanderDuration = Random.Range(MinWanderDuration, MaxWanderDuration);
+            _direction = new Vector3(_randX, 0f, _randZ).normalized;
+	        _speed = .025f;
             _elapsedWait = 0;
 	        _isMoving = true;
 	        StopAllCoroutines();
@@ -46,11 +58,12 @@ public class WanderScript : MonoBehaviour {
         /* otherwise if the actor is moving and their walk time has reached
          * its maximum, start the waiting process and halt their movement
          * additionally, reset the random direction */
-	    else if (_isMoving && _elapsedWalk >= _maxWanderDuration)
+	    else if (_isMoving && _elapsedWander >= _wanderDuration)
 	    {
-	        _direction = Vector3.zero;
+	        _waitDuration = Random.Range(MinWaitDuration, MaxWaitDuration);
+            _direction = Vector3.zero;
 	        _speed = 0;
-	        _elapsedWalk = 0;
+	        _elapsedWander = 0;
 	        _isMoving = false;
 	        _randX = Random.Range(-1f, 1f);
 	        _randZ = Random.Range(-1f, 1f);
@@ -66,15 +79,15 @@ public class WanderScript : MonoBehaviour {
          * one of the time counters */
         if (which)
         {
-            while (_elapsedWalk < _maxWanderDuration)
+            while (_elapsedWander < _wanderDuration)
             {
-                _elapsedWalk += 1;
+                _elapsedWander += 1;
                 yield return new WaitForSeconds(1f);
             }
         }
         else
         {
-            while (_elapsedWait < _maxWaitTime)
+            while (_elapsedWait < _waitDuration)
             {
                 _elapsedWait += 1;
                 yield return new WaitForSeconds(1f);
